fbcbf922d0311deb10dea63b86fa33100b6bb216
Guwenk <denis180799@gmail.com>
2018-09-14 13:14:45 +0300

    test

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,12 +4,21 @@ class TodosController < ApplicationController
 		params = project_params
 		todo = Todo.new(text: params[:text], isCompleted: false, project_id: params[:project_id])
 		todo.save
-		redirect_to root_path
+		respond_to do |format|
+		    msg = { :status => "ok", :message => "Success!"}
+		    format.json  { render :json => msg } # don't do msg.to_json
+		    format.html {redirect_to root_path}
+		end
 	end
 
 	def toggle
 		Todo.update(params[:todo_id], isCompleted: params[:isCompleted])
 		puts params[:todo_id]
+		respond_to do |format|
+			msg = { :status => "ok", :message => "Success!"}
+			format.json  { render :json => msg } # don't do msg.to_json
+			format.html
+		end
 	end
 
 	private def project_params
